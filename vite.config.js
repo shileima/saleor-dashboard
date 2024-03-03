c3cf83bb50681290d26d2c98421cf9cfc2bf29cb
Loading <604237073@qq.com>
2024-03-03 17:36:08 +0800

    Update vite.config.js
    update output dir from "../dist/dashboard" to "../dist"

--- vite.config.js
+++ vite.config.js
@@ -164,7 +164,7 @@ export default defineConfig(({ command, mode }) => {
       sourcemap,
       minify: false,
       emptyOutDir: true,
-      outDir: "../dist/dashboard",
+      outDir: "../dist",
       assetsDir: ".",
       commonjsOptions: {
         /*
